5c590dfb041ab0c5488518d9a9bc021ba1d5f6a3
JaydannG <grossmanjaydann@gmail.com>
2026-09-22 18:24:22 -0400

    finished createHand tests

--- poker-lab/src/poker/engine/createHand.test.ts
+++ poker-lab/src/poker/engine/createHand.test.ts
@@ -5,7 +5,7 @@ import createHand from "./createHand";
 describe("createHand", () => {
     it("Should create a new hand with 6 players", () => {
         const hand = createHand();
-        expect(hand.players.length === 6);
+        expect(hand.players.length).toBe(6);
     });
 
     it("Should initialize the correct positions", () => {
@@ -21,9 +21,45 @@ describe("createHand", () => {
         ]);
     });
 
+    it("Should initialize the correct stack sizes (including posted blinds)", () => {
+        const hand = createHand();
+        const stacks = hand.players.map(player => player.stack);
+        expect(stacks).toEqual([
+            99.5,
+            99,
+            100,
+            100,
+            100,
+            100
+        ]);
+    });
+
+    it("Should initialize the pot correctly (including posted blinds)", () => {
+        const hand = createHand();
+        expect(hand.pot).toBe(1.5);
+    });
+
+    it("Should initalize all players as unfolded", () => {
+        const hand = createHand();
+        const folded = hand.players.map(player => player.folded);
+        expect(folded).toEqual([
+            false,
+            false,
+            false,
+            false,
+            false,
+            false
+        ])
+    });
+
+    it("Should initialize an empty action history", () => {
+        const hand = createHand();
+        expect(hand.actions).toEqual([]);
+    });
+
     it("Should advance action to the next active player", () => {
         const hand = createHand();
         const nextActivePlayer = getNextActivePlayer(hand);
-        expect(nextActivePlayer === "SB");
+        expect(nextActivePlayer).toBe("MP");
     });
 });
