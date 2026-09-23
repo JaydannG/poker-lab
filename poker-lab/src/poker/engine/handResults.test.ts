4cd07548bad328c6ec5d33ed3213b36fdd0c030b
JaydannG <grossmanjaydann@gmail.com>
2026-09-23 09:30:24 -0400

    getWinner tests completed

--- poker-lab/src/poker/engine/handResults.test.ts
+++ poker-lab/src/poker/engine/handResults.test.ts
@@ -2,9 +2,9 @@ import { getNextActivePlayer, applyAction } from "./applyAction";
 import createHand from "./createHand";
 import { Action, Position, ActionType } from "../types";
 import { describe, it, expect } from "vitest";
-import { isHandOver } from "./handResults";
+import { isHandOver, getWinner } from "./handResults";
 
-describe("handResults", () => {
+describe("isHandOver", () => {
     it("Should not end hand if all 6 players remain", () => {
         const hand = createHand();
 
@@ -127,12 +127,26 @@ describe("handResults", () => {
     it("Should error if no players remain", () => {
         const hand = createHand();
 
+        hand.players.forEach(player => player.folded = true);
+
+        expect(() => isHandOver(hand)).toThrow("No players in the hand");
+    });
+});
+
+describe("getWinner", () => {
+    it("Should return null for 6 players remaining", () => {
+        const hand = createHand();
+
+        expect(getWinner(hand)).toEqual(null);
+    });
+
+    it("Should return null for 2 players remaining", () => {
+        const hand = createHand();
+
         const utg = hand.players.find(player => player.position === "UTG");
         const mp = hand.players.find(player => player.position === "MP");
         const co = hand.players.find(player => player.position === "CO");
         const btn = hand.players.find(player => player.position === "BTN");
-        const sb = hand.players.find(player => player.position === "SB");
-        const bb = hand.players.find(player => player.position === "BB");
 
         if (utg) {
             utg.folded = true;
@@ -150,14 +164,42 @@ describe("handResults", () => {
             btn.folded = true;
         }
 
-        if (sb) {
-            sb.folded = true;
-        }
+        expect(getWinner(hand)).toEqual(null);
+    });
 
-        if (bb) {
-            bb.folded = true;
-        }
+    it("Should return UTG if only UTG remains", () => {
+        const hand = createHand();
 
-        expect(() => isHandOver(hand)).toThrow("No players in the hand");
+        hand.players.map(player => {
+            if (player.position != "UTG") {
+                player.folded = true;
+            }
+        });
+
+        expect(getWinner(hand)).toEqual("UTG");
+    });
+
+    it("Should return BTN if only BTN remains", () => {
+        const hand = createHand();
+
+        hand.players.map(player => {
+            if (player.position != "BTN") {
+                player.folded = true;
+            }
+        });
+
+        expect(getWinner(hand)).toEqual("BTN");
+    });
+
+    it("Should return BB if only BB remains", () => {
+        const hand = createHand();
+
+        hand.players.map(player => {
+            if (player.position != "BB") {
+                player.folded = true;
+            }
+        });
+
+        expect(getWinner(hand)).toEqual("BB");
     });
 });
